80327df2b29d95df6a8466360d614751f4fa6e6f
rpoltorakov <rpoltorakov@gmail.com>
2023-08-10 23:30:13 +0300

    fix: ошибки первого ревью

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -19,58 +19,58 @@ function Profile({ isOpened, onClose, onClick }) {
 
   return (
     <div className='profile'>
-
       <Header onClick={onClick} />
+      <Popup isOpened={isOpened} onClose={onClose} />
+      
+      <form className='profile__section'>
 
-      <main className="profile__content">
-        <Popup isOpened={isOpened} onClose={onClose} />
-        <form className='profile__section'>
         <h2 className='profile__title'>Привет, Роман!</h2>          
 
-          <div className="profile__wrapper">
-            <label className="profile__inputName profile__input_bordered" htmlFor='profileName'>Имя</label>
-            <input
-              id='profileName'
-              className="profile__input profile__input_bordered"
-              placeholder='Роман'
-              readOnly={!editMode}
-              minLength='2'
-              maxLength='200'
-              required
-            />
-            <label className="profile__inputName" htmlFor='profileEmail'>E-mail</label>
-            <input
-              id='profileEmail'
-              className="profile__input"
-              placeholder='pochta@yandex.ru'
-              readOnly={!editMode}
-              minLength='2'
-              maxLength='200'
-              required
-            />
-          </div>
+        <div className="profile__wrapper">
+          <label className="profile__inputName profile__input_bordered" htmlFor='profileName'>Имя</label>
+          <input
+            id='profileName'
+            className="profile__input profile__input_bordered"
+            placeholder='Роман'
+            readOnly={!editMode}
+            minLength='2'
+            maxLength='200'
+            required
+          />
+          <label className="profile__inputName" htmlFor='profileEmail'>E-mail</label>
+          <input
+            id='profileEmail'
+            className="profile__input"
+            placeholder='pochta@yandex.ru'
+            readOnly={!editMode}
+            minLength='2'
+            maxLength='200'
+            required
+          />
+        </div>
 
-          <div className="profile__buttons">
-            {
-              editMode ? 
-                (
-                  <button
-                    className="profile__button profile__saveButton"
-                    onClick={handleProfileSaveButton}
-                  >Сохранить</button>
-                )
-                : (
-                  <button 
-                    className="profile__button profile__editButton"
-                    onClick={handleProfileEditButton}
-                  >Редактировать</button>
-                )
-            }
-            <button className="profile__button profile__signoutButton">Выйти из аккаунта</button>
-          </div>
-        </form>
+        <div className="profile__buttons">
+          {
+            editMode ? 
+              (
+                <button
+                  className="profile__button profile__saveButton"
+                  onClick={handleProfileSaveButton}
+                >Сохранить</button>
+              )
+              : (
+                <button 
+                  className="profile__button profile__editButton"
+                  onClick={handleProfileEditButton}
+                >Редактировать</button>
+              )
+          }
+          <button className="profile__button profile__signoutButton">Выйти из аккаунта</button>
+        </div>
 
-      </main>
+      </form>
+
+
     </div>
   );
 }
